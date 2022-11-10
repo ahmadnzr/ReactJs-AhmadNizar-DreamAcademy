1088d6340d6a69d5db364e2d83e4f8d8609aa586
Ahmad Nizar <ahmad.rpl001@gmail.com>
2022-11-11 00:58:04 +0700

    assignment 04: number 03

---
+++ 04-javascript-advance-1/soal-03/main.js
@@ -0,0 +1,33 @@
+const result = document.getElementById("note");
+const numberg = document.getElementById("number");
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+
+let number;
+let undi;
+
+const startUndian = () => {
+  result.innerHTML = "";
+  numberg.innerHTML = "0";
+  if (!undi) {
+    undi = setInterval(() => {
+      number = Math.round(Math.random() * 99);
+      numberg.innerHTML = number;
+    }, 30);
+  }
+};
+
+const stopUndian = () => {
+  clearInterval(undi);
+  undi = null;
+
+  const menang = number % 2 !== 0;
+  if (menang) {
+    result.innerHTML = "MENANG";
+    return;
+  }
+  result.innerHTML = "KALAH";
+};
+
+start.addEventListener("click", startUndian);
+stop.addEventListener("click", stopUndian);
