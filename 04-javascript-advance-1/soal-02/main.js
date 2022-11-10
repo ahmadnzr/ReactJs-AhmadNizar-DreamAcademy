9e66c3696b79d219e402b7615267679f2e304e90
Ahmad Nizar <ahmad.rpl001@gmail.com>
2022-11-11 00:27:54 +0700

    assignment 04: number 02

---
+++ 04-javascript-advance-1/soal-02/main.js
@@ -0,0 +1,52 @@
+const form = document.querySelector("form");
+const iName = document.querySelectorAll("input")[0];
+const iDate = document.querySelectorAll("input")[1];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const { age, birthdayIn } = info(iDate.value);
+  const { age: mAge, birthdayIn: mBirthDayIn } = infoWithMoment(iDate.value);
+
+  console.log("object Date =>\n", getBirthDay(age, birthdayIn));
+  console.log("moment =>\n", getBirthDay(mAge, mBirthDayIn));
+});
+
+const getBirthDay = (age, birthday) => {
+  const birthdayIn =
+    birthday > 0
+      ? `${Math.abs(birthday)} hari lagi`
+      : `${Math.abs(birthday)} hari yang lalu`;
+
+  return `Halo, ${iName.value}. Usia anda adalah ${age} tahun. Anda berulangan tahun ${birthdayIn}`;
+};
+
+const info = (date) => {
+  const now = new Date();
+  const born = new Date(date);
+  const birthday = new Date(
+    now.getFullYear(),
+    born.getMonth(),
+    born.getDate()
+  ).getTime();
+
+  return {
+    age: new Date(now - born.getTime()).getUTCFullYear() - 1970,
+    birthdayIn: Math.trunc((birthday - now.getTime()) / (1000 * 3600 * 24)),
+  };
+};
+
+const infoWithMoment = (date) => {
+  const now = moment();
+  const born = moment(date);
+
+  const birthday = moment({
+    year: now.year(),
+    month: born.month(),
+    date: born.date(),
+  });
+
+  return {
+    age: now.diff(born, "year"),
+    birthdayIn: birthday.diff(now, "day"),
+  };
+};
